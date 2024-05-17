283b0b3698325c70cc737465e6a13ec56632b49d
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-17 17:02:52 +0200

    created cicle 'for' to create 100 box inside container

--- js/script.js
+++ js/script.js
@@ -3,4 +3,12 @@
 const gridContainer = document.getElementsByClassName('container');
 // console.log(gridContainer);
 
-
+// creo un ciclo 'for' per inserire 100 elementi al interno del container
+for (let index = 0; index < 100; index++) {
+    // creo un elemento 
+    const squareEl = document.createElement('article');
+    // aggiungo una classe a squareEl
+    squareEl.classList.add('box');
+    gridContainer.appendChild(squareEl);
+    
+}
